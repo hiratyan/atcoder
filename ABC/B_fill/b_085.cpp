e1f9dfed8303ecf70fe0764d356fe14f6bc6eeb2
hiratyan <monokeshi5121@gmail.com>
2022-08-18 16:13:15 +0900

    add

--- ABC/B_fill/b_085.cpp
+++ ABC/B_fill/b_085.cpp
@@ -2,13 +2,29 @@
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+#define rep(i, s, n) for (int i = (int)(s); i < (int)(n); i++)
+#define rep0(i, n) for (int i = 1; i < (int)(n); i++)
 using ll = long long;
 using P = pair<int,int>;
 using Graph = vector<vector<int>>;
 using mint = modint1000000007;
 
+int main(){
+    int n, c = 1;
+    cin >> n;
+    vector<int> vec(n);
+    rep(i, 0, n) cin >> vec.at(i);
+    sort(vec.begin(), vec.end());
+    for(int i = 0; i < n - 1; i++){
+        if(vec.at(i) != vec.at(i + 1)){
+            c++;
+        }
+    }
+    cout << c << endl;
+}
+
+
+/*
 int main() {
     int n, c = 1;
     cin >> n;
@@ -31,3 +47,4 @@ int main() {
 
     return 0;
 }
+*/
