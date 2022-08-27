e6007bb4f6608df868068a3a33cd19b5a2ffb8d7
hiratyan <monokeshi5121@gmail.com>
2022-08-27 21:45:40 +0900

    add

---
+++ 4AnotherContest/mituisumitomo2019/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, s, n) for (int i = (int)(s); i < (int)(n); i++)
+#define rep0(i, n) for (int i = 1; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+const ll INF = 1LL << 60; //無限大
+const ll MOD = 1000000007; //10^9 + 7
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        if(i * 108 / 100 == n){
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << ":(" << endl;
+    return 0;
+}
