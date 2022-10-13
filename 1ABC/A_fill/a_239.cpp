87141e990b0571b7c6ca55ae7699972fe8171104
hiratyan <monokeshi5121@gmail.com>
2022-10-13 15:14:09 +0900

    add

---
+++ 1ABC/A_fill/a_239.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, s, n) for (int i = (int)(s); i < (int)(n); i++)
+#define rep0(i, n) for (int i = 1; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+const ll INF = 1LL << 60; //無限大
+const ll MOD = 1000000007; //10^9 + 7
+
+int main() {
+    double h, ans;
+    cin >> h;
+    ans = sqrt(h * (12800000 + h));
+    cout << fixed << setprecision(10) << ans << endl;
+
+    return 0;
+}
